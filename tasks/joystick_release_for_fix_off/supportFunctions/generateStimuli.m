be9556bcd8793863c7421e2285f2f9411d014f29
James P. Herman <hermanj@gmail.com>
2022-09-01 10:47:36 -0400

    Added new task "joystick_release_on_fix_off"

---
+++ tasks/joystick_release_for_fix_off/supportFunctions/generateStimuli.m
@@ -0,0 +1,10 @@
+function p = generateStimuli(p)
+
+% p = generateStimuli(p)
+%
+% generate stimuli
+
+% use stimSeed to determine following pseudorandom number sequence
+% rng(p.trVars.stimSeed);
+
+end
