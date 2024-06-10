21ad9a6f238aaad8dbac376e66178dc4183cc895
matthewmurawski2 <124393365+matthewmurawski2@users.noreply.github.com>
2024-06-10 16:29:12 -0400

    Update joystick_release_for_orient_change_and_dim_learn_cue_settings.m
    Added initialization of total end trial state trackers

--- tasks/joystick_release_for_stim_change_and_dim/joystick_release_for_orient_change_and_dim_learn_cue_settings.m
+++ tasks/joystick_release_for_stim_change_and_dim/joystick_release_for_orient_change_and_dim_learn_cue_settings.m
@@ -180,6 +180,12 @@ p.status.foil2CtLoc1                = 0; % count of two patch foil change trials
 p.status.cue2CtLoc2                 = 0; % count of two patch cue change trials at location 2
 p.status.foil2CtLoc2                = 0; % count of two patch foil change trials at location 2
 
+p.status.totalHits                  = 0; % count of correct rejects
+p.status.totalMisses                = 0; % count of misses
+p.status.totalChangeFalseAlarms     = 0; % count of change FAs
+p.status.totalNoChangeFalseAlarms   = 0; % count of no change FAs
+p.status.totalCorrectRejects        = 0; % count of correct rejects
+
 p.status.missedFrames               = 0; % count of missed frames as reported by psychtoolbox
 p.status.freeRwdRand                = 0; % random number drawn for deciding whether or not to deliver free reward
 p.status.freeRwdTotal               = 0; % total count of free inter trial interval rewards delivered
